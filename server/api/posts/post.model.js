1eebd0aecc112ecee232f9c6eb3e9df063c926b0
Aibhne Breathnach <aibhnebreathnach@gmail.com>
2017-11-28 11:55:49 +0000

    Updated post model with user and location associations

--- server/api/posts/post.model.js
+++ server/api/posts/post.model.js
@@ -4,9 +4,9 @@ var Schema = mongoose.Schema;
 var PostSchema = new Schema({
   title : { type : String, required: true },
   imageLink : { type : String, required: true },
-  description : { type : String, required: true }
-//   user : [{ type: Schema.Types.ObjectId, ref: 'User', required : true}],
-//   location : [{ type : Schema.Types.ObjectId, ref : 'Location', required : true}]
+  description : { type : String, required: true },
+  user : [{ type: Schema.Types.ObjectId, ref: 'User', required : true}],
+  location : [{ type : Schema.Types.ObjectId, ref : 'Location', required : true}]
 });
 
 module.exports = mongoose.model('posts', PostSchema);
